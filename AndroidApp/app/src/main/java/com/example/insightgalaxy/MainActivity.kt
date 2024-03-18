9d754d1f9d6978cf2e568617e6d0cb2447583617
Romain BOURGET <E225486T@ubuntu.srv-podman-2>
2024-03-18 10:56:51 +0100

    création projet android et début de vue principale

---
+++ AndroidApp/app/src/main/java/com/example/insightgalaxy/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.insightgalaxy
+
+import android.app.ActionBar
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
