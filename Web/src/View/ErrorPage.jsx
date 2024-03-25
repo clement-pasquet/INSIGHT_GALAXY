e35c255dc47965b834726292e147e3ebd3a3fd1e
Bryan Levy <bryan.levy@etu.univ-nantes.fr>
2024-03-25 17:49:14 +0100

    Début implémentation page erreur

--- Web/src/View/ErrorPage.jsx
+++ Web/src/View/ErrorPage.jsx
@@ -3,13 +3,23 @@ import {setStyle} from "../Controller/App"
 
 
 export function ErrorPage(){
-    setStyle({styles : ["/src/Style/index.css"]}); //Nous permet de définir un style spécial pour chaque page
+    setStyle({styles : ["/src/Style/index.css","/src/Style/ErrorPage.css"]}); //Nous permet de définir un style spécial pour chaque page
 
     const error = useRouteError()
-    return <div>
+    return <><div className="errorPart">
+            <h1>O<img className="planetImg" src="/src/assets/ErrorPlanet.png"></img>OPS</h1>
+
+        
         La page que vous essayer de rejoindre n'existe pas !
         <p>
-            {error?.error?.toString() ?? error?.toString()}
+            {console.log(error)}
         </p>
+        <img className="errorIllustration" src="/src/assets/illustrationErrorPage.svg"></img>
+
     </div>
+    </>
+}
+
+function m() {
+
 }
