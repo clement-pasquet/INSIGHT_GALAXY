2fed458debe1083756d01a6acd129a1837e319ef
E22A210W <clement.pasquet@etu.univ-nantes.fr>
2024-03-25 20:43:17 +0100

    Remodelage totale page erreur

--- Web/src/View/ErrorPage.jsx
+++ Web/src/View/ErrorPage.jsx
@@ -6,20 +6,30 @@ export function ErrorPage(){
     setStyle({styles : ["/src/Style/index.css","/src/Style/ErrorPage.css"]}); //Nous permet de définir un style spécial pour chaque page
 
     const error = useRouteError()
-    return <><div className="errorPart">
-            <h1>O<img className="planetImg" src="/src/assets/ErrorPlanet.png"></img>OPS</h1>
+    return <>
+    <div className="errorView">
+        <div className="errorPart">
 
-        
-        La page que vous essayer de rejoindre n'existe pas !
-        <p>
-            {console.log(error)}
-        </p>
+            <h1>O<img className="planetImg" src="/src/assets/ErrorPlanet.png"></img>OPS !</h1>
+
+            
+            <div className="textDiv">
+                <p className="errorCode">{error.status} {error.statusText}</p>
+
+                {/* console.log(error) */}
+
+                <p className="errorDescription">
+                    {error.data}
+                </p>
+
+
+                <button onClick={() => window.location.href = "/Home"} className="homeButton">Go Home</button>
+            </div>
+
+
+        </div>
+
         <img className="errorIllustration" src="/src/assets/illustrationErrorPage.svg"></img>
-
     </div>
     </>
-}
-
-function m() {
-
 }
