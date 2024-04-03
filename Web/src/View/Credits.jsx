7725c04f6e2dbeb5d2a3f8addb2fc4703a23809b
Basma MALKI <E22C000R@ubuntu.srv-podman-2>
2024-04-03 16:14:13 +0200

    Search Page modified

--- Web/src/View/Credits.jsx
+++ Web/src/View/Credits.jsx
@@ -1,23 +1,25 @@
-import {setStyle} from "../Controller/App"
-
+import { setStyle } from "../Controller/App";
 
 export function Credits() {
-    setStyle({styles : ["/src/Style/index.css"]}); //Nous permet de définir un style spécial pour chaque page
+  setStyle({ styles: ["/src/Style/Credit.css"] }); //Nous permet de définir un style spécial pour chaque page
 
-    let developpeur = [{ name: 'Bryan Levy', age: 19, description: "", linkedin: '', gitlab: '' },{ name: 'Basma Malki', age: 19, description: "", linkedin: '', gitlab: '' },{ name: 'Clément Pasquet', age: 19, description: "", linkedin: '', gitlab: '' },{ name: 'Justine Bernier', age: 19, description: "", linkedin: '', gitlab: '' },{ name: 'Romain Bourget', age: 19, description: "", linkedin: '', gitlab: '' }];
-  
-    return (
-      <div>
-        <h1>Les crédits</h1>
-        <p>Ce site a été réalisé par 5 développeurs, étudiants en BUT informatique</p>
-        <div>
-          {developpeur.map((element, index) => (
-            <div key={index}>
-              {element.name}
-            </div>
-          ))}
+  let developpeur = [
+    { name: 'Bryan Levy', age: 19, description: "", linkedin: '', gitlab: '' },
+    { name: 'Basma Malki', age: 19, description: "", linkedin: '', gitlab: '' },
+    { name: 'Clément Pasquet', age: 19, description: "", linkedin: '', gitlab: '' },
+    { name: 'Justine Bernier', age: 19, description: "", linkedin: '', gitlab: '' },
+    { name: 'Romain Bourget', age: 19, description: "", linkedin: '', gitlab: '' }
+  ];
+
+  return (
+    <div className="team-container">
+      {developpeur.map((dev, index) => (
+        <div className="team-member" key={index}>
+          <img src={`${dev.name.toLowerCase()}.jpg`} alt={dev.name} />
+          <h3>{dev.name}</h3>
+          <a href="#"><img src="linkedin.png" alt="LinkedIn" /></a>
         </div>
-      </div>
-    );
-  }
-  
+      ))}
+    </div>
+  );
+}
