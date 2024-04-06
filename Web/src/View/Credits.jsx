7b6ca99e01ddca952464e552eaf7e5e42935985d
E22A210W <clement.pasquet@etu.univ-nantes.fr>
2024-04-06 23:52:39 +0200

    Ajout page Crédit et Style

--- Web/src/View/Credits.jsx
+++ Web/src/View/Credits.jsx
@@ -4,22 +4,38 @@ export function Credits() {
   setStyle({ styles: ["/src/Style/Credit.css"] }); //Nous permet de définir un style spécial pour chaque page
 
   let developpeur = [
-    { name: 'Bryan Levy', age: 19, description: "", linkedin: '', gitlab: '' },
-    { name: 'Basma Malki', age: 19, description: "", linkedin: '', gitlab: '' },
-    { name: 'Clément Pasquet', age: 19, description: "", linkedin: '', gitlab: '' },
-    { name: 'Justine Bernier', age: 19, description: "", linkedin: '', gitlab: '' },
-    { name: 'Romain Bourget', age: 19, description: "", linkedin: '', gitlab: '' }
+    { name: 'Bryan', lastname:'Levy', age: 19, description: "", linkedin: '', gitlab: '' },
+    { name: 'Basma', lastname:'Malki', age: 19, description: "", linkedin: '', gitlab: '' },
+    { name: 'Clément', lastname:'Pasquet', age: 19, description: "", linkedin: '', gitlab: '' },
+    { name: 'Justine', lastname:'Bernier', age: 19, description: "", linkedin: '', gitlab: '' },
+    { name: 'Romain', lastname:'Bourget', age: 19, description: "", linkedin: '', gitlab: '' }
   ];
 
   return (
-    <div className="team-container">
-      {developpeur.map((dev, index) => (
-        <div className="team-member" key={index}>
-          <img src={`${dev.name.toLowerCase()}.jpg`} alt={dev.name} />
-          <h3>{dev.name}</h3>
-          <a href="#"><img src="linkedin.png" alt="LinkedIn" /></a>
-        </div>
-      ))}
+    <div className="containerBox">
+
+      <div className="centralBox">
+
+        {developpeur.map( (developpeurActuel, cle) => {
+          return (
+          <div key={cle} className="profileDiv">
+            <img src={"/src/assets/"+developpeurActuel.name+" "+developpeurActuel.lastname+".jpg"} className="profileImage"></img>
+            <div>
+            <h2>{developpeurActuel.name}</h2>
+            <h2>{developpeurActuel.lastname}</h2>
+            </div>
+            <img src="/src/assets/linkedin.svg" className="iconImage"></img>
+            <div className="gitDiv">
+              <img src="/src/assets/gitlab.svg" className="iconImage"></img>
+              <img src="/src/assets/github.svg" className="iconImage"></img>
+            </div>
+          </div>
+          
+          );
+        }) }
+
+      </div>
+
     </div>
   );
 }
